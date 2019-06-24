30580df7b2f0fbc98c58c47bb0596ed3013d244d
miyakelp <lisknkknk.al.g2k13+miyakelp@gmail.com>
2019-06-24 16:11:10 +0000

    load a file into memory

---
+++ core/memory.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "memory.h"
+
+
+Memory *memory_create(uint32_t size) {
+  Memory *memory = (Memory *)malloc(sizeof(Memory));
+  if (memory == NULL) {
+    return NULL;
+  }
+  memory->memory = (uint8_t *)malloc(sizeof(uint8_t) * size);
+  if (memory->memory == NULL) {
+    return NULL;
+  }
+  memory->size = size;
+  return memory;
+}
+
+
+int memory_load_file(Memory *memory, FILE *file, uint32_t address) {
+  int count = fread(memory->memory + address, 1, memory->size - address, file);
+  fclose(file);
+
+  if (count == memory->size - address) { 
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
--- core/memory.h
+++ core/memory.h
@@ -1,6 +1,21 @@
 #ifndef _MEMORY_H
 #define _MEMORY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define DEFAULT_MEMORY_SIZE 0xffff;
+
+typedef struct Memory {
+  uint32_t size;
+  uint8_t *memory;
+} Memory;
+
+
+Memory *memory_create(uint32_t);
+int memory_load_file(Memory *, FILE *, uint32_t);
+
 
 #endif
 
